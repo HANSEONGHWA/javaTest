259706360e67e816322542005ff749f1d028cf87
HANSEONGHWA <ghk9206@hanmail.net>
2023-04-04 12:56:41 +0900

    중첩 인터페이스

---
+++ src/javajungsuk/ch7/Ch7_10.java
@@ -0,0 +1,50 @@
+package javajungsuk.ch7;
+
+class Button{
+    interface OnClickListener{
+        void onClick();
+    }
+    OnClickListener listener;
+
+    void setOnClickListener(OnClickListener listener){
+        this.listener = listener;
+    }
+
+    void touch(){
+        listener.onClick();
+    }
+
+}
+
+class CallListener implements Button.OnClickListener{
+
+    @Override
+    public void onClick() {
+        System.out.println("전화를 겁니다.");
+    }
+}
+
+class MessageListener implements Button.OnClickListener{
+
+    @Override
+    public void onClick() {
+        System.out.println("메세지를 보냅니다.");
+    }
+}
+
+
+public class Ch7_10 {
+    public static void main(String[] args) {
+
+        Button bnt = new Button();
+
+        bnt.setOnClickListener(new CallListener());
+        bnt.touch();
+
+        bnt.setOnClickListener(new MessageListener());
+        bnt.touch();
+    }
+
+
+
+}
