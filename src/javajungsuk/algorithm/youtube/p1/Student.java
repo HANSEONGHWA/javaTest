7b6f81a171115a55fe65be2aa1559988429ac4fb
HANSEONGHWA <ghk9206@hanmail.net>
2023-04-08 04:16:24 +0900

    알고리즘 기초 문제 1. 학생이름 저장 및 검색

---
+++ src/javajungsuk/algorithm/youtube/p1/Student.java
@@ -0,0 +1,29 @@
+package javajungsuk.algorithm.youtube.p1;
+
+public class Student {
+    private String neme;
+    private String no;
+
+    public Student(String neme, String no) {
+        this.neme = neme;
+        this.no = no;
+    }
+
+
+    public String getNeme() {
+        return neme;
+    }
+
+    public void setNeme(String neme) {
+        this.neme = neme;
+    }
+
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
+
+}
